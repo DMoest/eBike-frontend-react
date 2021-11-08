aa1dc039f6654898a4b96c3a1c4231437a81b7bd
Robin Granqvist <robin.granqvist@hotmail.se>
2021-11-08 17:40:04 +0000

    Added things

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-// import './App.css';
+import './css/App.css';
+import './css/Other.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import City from './pages/City'
 import Bike from './pages/Bike'
 import Charging from './pages/Charging'
-import Map from './pages/Map'
-
 
 function App() {
   return (
@@ -17,7 +16,6 @@ function App() {
         <Route path="/city" element={ <City /> }/>
         <Route path="/bike" element={ <Bike /> }/>
         <Route path="/charging" element={ <Charging />}/>
-        <Route path="/map" element={ <Map /> }/>
       </Routes>
     </div>
   );
---
+++ src/components/BtnPrimary.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BtnPrimary({ btnText }) {
+    return (
+        <button className="btn__primary">
+            { btnText }
+        </button>
+    )
+}
+
+export default BtnPrimary
