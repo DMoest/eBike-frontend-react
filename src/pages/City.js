bde4715ddb1e3d07c59ca594ae2f648bae35950e
Robin Granqvist <robin.granqvist@hotmail.se>
2021-11-09 19:06:18 +0000

    Added city child route, trying to pass data downn to the children, BUT HOW?

--- src/pages/City.js
+++ src/pages/City.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Header from '../components/global/Header'
 
-function City({ city = 'Stockholm' }) {
+function City({ title }) {
     return (
         <div className="wrapper">
-            <Header title={ city } />
+            <Header title={ title } />
         </div>
     )
 }
