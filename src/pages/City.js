cebd22f9ccef43bba6825dd1fd1cd35ad9a71109
Robin Granqvist <robin.granqvist@hotmail.se>
2021-11-09 21:54:24 +0000

    Added back button component

--- src/pages/City.js
+++ src/pages/City.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Header from '../components/global/Header'
+import BtnBack from '../components/global/BtnBack'
+
 import { useLocation } from 'react-router-dom'
 
 function City() {
@@ -9,6 +11,7 @@ function City() {
 
     return (
         <div className="wrapper">
+            <BtnBack url={ "cities" } />
             <Header title={ cityName } />
         </div>
     )
