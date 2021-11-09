5c3a133765abf466a6e29f59b21bcdb738b1745b
Robin Granqvist <robin.granqvist@hotmail.se>
2021-11-09 13:52:07 +0000

    Added mock-data for bikes, needs updating

--- src/components/City.js
+++ src/components/City.js
@@ -2,15 +2,20 @@ import React from 'react'
 import '../css/City.css'
 import map from '../assets/img/city__map-test.png'
 
-function City() {
+function City({ name, desc, bikes, charge, customers }) {
     return (
         <div className="city__outer-wrapper">
             <div className="city__img-wrapper">
                 <img src={ map } alt="test"/>
             </div>
             <div className="city__content-wrapper">
-                <h3>Stockholm</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ullamcorper quam eu</p>
+                <h3>{ name }</h3>
+                <p>{ desc }</p>
+                <div class="city__stats-wrapper">
+                    { bikes }
+                    { charge }
+                    { customers }
+                </div>
             </div>
         </div>
     )
--- src/pages/Cities.js
+++ src/pages/Cities.js
@@ -5,10 +5,42 @@ import Header from '../components/global/Header'
 import '../css/App.css'
 
 function Cities() {
+    const cities = [
+        {
+            name: 'Stockholm',
+            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ullamcorper quam eu',
+            bikes: 272,
+            charge: 13,
+            customers: 42
+        },
+        {
+            name: 'Stockholm',
+            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ullamcorper quam eu',
+            bikes: 272,
+            charge: 13,
+            customers: 42
+        },
+        {
+            name: 'Stockholm',
+            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ullamcorper quam eu',
+            bikes: 272,
+            charge: 13,
+            customers: 42
+        }
+    ]
+
     return (
         <div className="wrapper">
             <Header title="Städer"/>
-            <City />
+            { cities.map((bike) => {
+                return <City 
+                    name={bike.name} 
+                    desc={bike.desc} 
+                    bikes={bike.bikes} 
+                    charge={bike.charge} 
+                    customers={bike.customers}
+                />
+            }) }
         </div>
     )
 }
