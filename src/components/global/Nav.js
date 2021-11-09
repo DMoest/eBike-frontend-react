d530a97dad4b0f1c82bdff7bdd81ce6c3758588a
Robin Granqvist <robin.granqvist@hotmail.se>
2021-11-09 19:33:30 +0000

    Added active nav link color change thingies

--- src/components/global/Nav.js
+++ src/components/global/Nav.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import '../../css/Nav.css'
 import '../../css/App.css'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 function Nav() {
     return (
         <div className="nav__outer-wrapper">
             <div className="nav__brand">eBike</div>
             <div className="nav__menu">
-                <Link to="/" className="nav__link">Hem</Link>
-                <Link to="/cities" className="nav__link">Städer</Link>
-                <Link to="/bikes" className="nav__link">Cyklar</Link>
-                <Link to="/charging" className="nav__link">Laddstationer</Link>
-                <Link to="/login" className="nav__link nav__btn">Logga in</Link>
+                <NavLink to="/" className="nav__link">Hem</NavLink>
+                <NavLink to="/cities" className="nav__link">Städer</NavLink>
+                <NavLink to="/bikes" className="nav__link">Cyklar</NavLink>
+                <NavLink to="/charging" className="nav__link">Laddstationer</NavLink>
+                <NavLink to="/login" className="nav__link nav__btn">Logga in</NavLink>
             </div>
         </div>
     )
