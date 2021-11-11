72008bd63ecfb2506621e20eb19a40e646cf69f1
Robin Granqvist <robin.granqvist@hotmail.se>
2021-11-11 12:58:17 +0000

    Added Leaflet map. Fixed all errors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,13 @@ import React from 'react'
 import '../css/Other.css'
 import '../css/App.css'
 import Header from '../components/global/Header'
+import Map from '../components/Map'
 
 function Home() {
     return (
         <div className="wrapper">
             <Header title="Hem"/>
+            <Map />
         </div>
     )
 }
