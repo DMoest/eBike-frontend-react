11fbf7df727a632fdda18ef87fddec0f8495de5a
Robin Granqvist <robin.granqvist@hotmail.se>
2021-11-17 10:41:43 +0000

    Installed axios & fetching bikes API-data from local backend

--- src/pages/Bikes.js
+++ src/pages/Bikes.js
@@ -1,49 +1,24 @@
 import React from 'react'
+import axios from "axios"
+
 import Bike from '../components/Bike'
 import Header from '../components/global/Header'
 import DocumentTitle from 'react-document-title'
 
-function Bikes() {
-    // Mock data for the grid
-    const bikes = [
-        {
-            id: 123,
-            city: 'Stockholm',
-            lat: '12.212313',
-            lan: '52.123123',
-            latest_travel: '12:41 - 11.3.2021',
-            parking: 'Inte tillgänglig',
-            status: 'Upptagen'
-        },
-        {
-            id: 124,
-            city: 'Stockholm',
-            lat: '13.212313',
-            lan: '53.123123',
-            latest_travel: '12:42 - 11.3.2021',
-            parking: 'Parkering #2',
-            status: 'Ledig'
-        },
-        {
-            id: 123,
-            city: 'Stockholm',
-            lat: '12.212313',
-            lan: '52.123123',
-            latest_travel: '12:41 - 11.3.2021',
-            parking: 'Inte tillgänglig',
-            status: 'Upptagen'
-        },
-        {
-            id: 124,
-            city: 'Stockholm',
-            lat: '13.212313',
-            lan: '53.123123',
-            latest_travel: '12:42 - 11.3.2021',
-            parking: 'Parkering #2',
-            status: 'Ledig'
-        },
-    ]
+import { useState, useEffect } from 'react'
 
+function Bikes() {
+    const url = "http://localhost:8000/api/bike"
+    const [bikes, setBikes] = useState([])
+
+    useEffect(() => {
+        axios.get(url).then((res) => {
+          setBikes(res.data.bikes);
+        });
+    }, []);
+
+    console.log(bikes)
+    
     return (
         <div className="wrapper">
             <DocumentTitle title='Cyklar' ></DocumentTitle>
@@ -53,13 +28,13 @@ function Bikes() {
             <input type="text" placeholder="Sök" className="input__search"></input>
 
             {bikes.map((bike) => {
-                return <Bike
-                    id={bike.id}
+                return <Bike key={bike._id}
+                    // id={bike._id}
                     city={bike.city}
-                    lat={bike.lat}
-                    lan={bike.lan}
-                    latest_travel={bike.latest_travel}
-                    parking={bike.parking}
+                    lat={bike.latitude}
+                    lan={bike.longitude}
+                    // latest_travel={bike.latest_travel}
+                    // parking={bike.parking}
                     status={bike.status}
                 />
             })}
