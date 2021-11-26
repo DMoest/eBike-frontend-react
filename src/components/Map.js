9be8776f5fce4e48dd92d3e19f7f57962131e7ad
Robin Granqvist <robin.granqvist@hotmail.se>
2021-11-26 13:28:29 +0000

    Map is now through a canvas element instead of SVG

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,7 +5,7 @@ import MapMarkers from './MapMarkers'
 function Map() {
     return (
         <div>
-			<MapContainer preferCanvas={true}  center={[-21.769985, 125.491771]} zoom={2} scrollWheelZoom={true}>
+			<MapContainer preferCanvas={true} center={[-21.769985, 125.491771]} zoom={2} scrollWheelZoom={true}>
 				<TileLayer
 					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
