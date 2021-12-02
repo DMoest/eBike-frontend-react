0a831b324134303dec491e926346815b916c8c14
sohe3409 <sofia.herelius@gmail.com>
2021-12-02 17:09:54 +0000

    Fixing Stripe

--- src/components/customer/payment/ShowPayment.js
+++ src/components/customer/payment/ShowPayment.js
@@ -5,7 +5,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import PaymentForm from "./PaymentForm"; // not implemented yet
 const url = "http://127.0.0.1:8000";
-const stripePromise = loadStripe('pk_test_51JxuwuKh30a9IIAiSICB1wjtE3TwULgIyarKPNsteitiGFFavpdy4bjcNZaqWA20p8u3AwvAIV6fW3VuC0jh9zL3006yJMCYqP');
+const stripePromise = loadStripe(process.env.STRIPE_KEY);
 
 class ShowPayment extends React.Component {
     constructor(props) {
