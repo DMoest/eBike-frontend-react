e13fdf97c4a000ca367340ae9ceab3d076fe93c4
sohe3409 <sofia.herelius@gmail.com>
2022-01-08 23:09:34 +0100

    simplifying Login component

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -10,38 +10,38 @@ class Login extends React.Component {
             login: 'register',
             register: 'login'
         };
-        this.button = {
-            login: 'registrera dig via github',
-            register: 'logga in via github'
-        };
     }
 
     onSubmit = (e) => {
         e.preventDefault();
         let url = "http://localhos:8000/auth/github";
         if (this.state.form === 'login') {
-            axios.post(`${url}`, {
-                // email: e.target.email.value,
-                // password: e.target.password.value,
+            axios.get(`${url}/oauth/authorize`).then((response) => {
+                this.setState({ user: response.data });
             })
             .then(response => {
                return response
             })
             .then(data => {
                console.log(data)
+               this.props.parentCallback(data.access_token);
             })
             .catch(error => {
                console.log(error)
             })
         } else {
-            axios.post(`${url}/register`, {
-                firstname: e.target.fname.value,
-                lastname: e.target.lname.value,
-                email: e.target.email.value,
-                password: e.target.password.value,
-                adress: e.target.adress.value,
-                postcode: e.target.postcode.value,
-                city: e.target.city.value,
+            axios.get(`${url}/register`).then((response) => {
+                this.setState({ user: response.data });
+            })
+            .then(response => {
+               return response
+            })
+            .then(data => {
+               console.log(data)
+               this.props.parentCallback(data.access_token);
+            })
+            .catch(error => {
+               console.log(error)
             })
         }
     }
@@ -50,13 +50,13 @@ class Login extends React.Component {
         console.log(this.props.user)
         return (
             <div className="container">
-            <div style={{transform: `translate(${this.state.form === 'login' ? 0 : -350}px, 0px)`}} className="form-div">
-                <p>Är du redan medlem?'</p>
-                <button className="change-btn" onClick={() => {this.setState({form: this.toggle[this.state.form]})}}>{this.button[this.state.form]}</button>
+            <div className="form-div">
+                <p style={{textAlign: "center"}}>Är du redan medlem?'</p>
+                <button className="change-btn" onClick={() => {this.setState({form: "login"})}}>Logga in</button>
             </div>
-                <div style={{transform: `translate(${this.state.form === 'login' ? 0 : -350}px, 0px)`}} className="button-div">
-                    <p>Har du inget konto? Registrera dig här!</p>
-                    <button className="change-btn" onClick={() => {this.setState({form: this.toggle[this.state.form]})}}>{this.button[this.state.form]}</button>
+                <div className="button-div">
+                    <p style={{textAlign: "center"}}>Har du inget konto? Registrera dig här!</p>
+                    <button className="change-btn" onClick={() => {this.setState({form: "register"})}}>Registrera dig</button>
                 </div>
             </div>
         );
